a31c661d3c692ac9c76bf6aa7d81b848684bec58
kimseonghun96 <tjd951753@naver.com>
2023-01-09 01:41:52 +0900

    미니블로그

--- my-app/src/chapter_05/Comment.jsx
+++ my-app/src/chapter_05/Comment.jsx
@@ -33,21 +33,21 @@ const styles = {
 };
 
 function Comment(props) {
-    return (
-        <div style={styles.wrapper}>
-            <div style={styles.imageContainer}>
-                <img
-                    src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"
-                    style={styles.image}
-                />
-            </div>
+  return (
+    <div style={styles.wrapper}>
+    <div style={styles.imageContainer}>
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"
+          style={styles.image}
+        />
+      </div>
 
-            <div style={styles.contentContainer}>
-                <span style={styles.nameText}>{props.name}</span>
-                <span style={styles.commentText}>{props.comment}</span>
-            </div>
-        </div>
-    );
+      <div style={styles.contentContainer}>
+        <span style={styles.nameText}>{props.name}</span>
+        <span style={styles.commentText}>{props.comment}</span>
+      </div>
+    </div>
+  );
 }
 
 export default Comment;
